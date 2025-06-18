c43bbc655e787632eaf99d92522883eec1a70f59
brokenhardware <brokenhardware@proton.me>
2025-06-18 09:10:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ const hashString = window.location.hash.substring(1);
 if (hashString) {
   const hashParams = new URLSearchParams(hashString);
   if (hashParams.has("html")) {
-    console.log("HTML CID:", hashParams.get("html"));
+    console.log("ipns html:", hashParams.get("html"));
     const ipnsAddr = hashParams.get("html");
     const response = await verifiedFetch('ipns://' + ipnsAddr)
     const html = await response.text()
     document.body.innerHTML = html
   }
+  if (hashParams.has("js")) {
+    console.log("ipfs js:", hashParams.get("js"));
+    const ipfsJSAddr = hashParams.get("js");
+    const response2 = await verifiedFetch('ipns://' + ipfsJSAddr)
+    const jscode = await response2.text()
+    const script = document.createElement('script');
+    script.src = jscode;
+    document.body.appendChild(script);
+  }
+
 }
 else {
   console.log ("Missing hash string");
