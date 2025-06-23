2403b5b01f93b0369ce04c9c4c44fa641390868c
brokenhardware <brokenhardware@proton.me>
2025-06-23 22:41:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -38,8 +38,47 @@ if (hashString) {
     //script.onload = () => URL.revokeObjectURL(blobUrl);
     document.body.appendChild(script);
   }
-
 }
 else {
   console.log ("Missing hash string");
+}
+
+const searchString = window.location.hash.substring(1);
+if (searchString) {
+  const searchParams = new URLSearchParams(searchString);
+  if (searchParams.has("css")) {
+    console.log("ipfs css:", searchParams.get("css"));
+    const ipfsCSSAddr = searchParams.get("css");
+
+    const response1 = await verifiedFetch('ipfs://' + ipfsCSSAddr);
+    const cssBlob = new Blob([await response1.text()], { type: 'text/css' });
+    const cssBlobUrl = URL.createObjectURL(cssBlob);
+    
+    //const response1 = await verifiedFetch('ipfs://' + ipfsCSSAddr)
+    //const cssBlob = await response1.blob();
+    //const cssBlobUrl = URL.createObjectURL(cssBlob);
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssBlobUrl;
+    link.type = 'text/css';
+    document.head.appendChild(link);
+  }
+  if (searchParams.has("html")) {
+    console.log("ipns html:", searchParams.get("html"));
+    const ipnsAddr = searchParams.get("html");
+    const response2 = await verifiedFetch('ipns://' + ipnsAddr)
+    const html = await response2.text()
+    document.body.innerHTML = html
+  }
+  if (searchParams.has("js")) {
+    console.log("ipfs js:", searchParams.get("js"));
+    const ipfsJSAddr = searchParams.get("js");
+    const response3 = await verifiedFetch('ipfs://' + ipfsJSAddr)
+    const blob = await response3.blob();
+    const blobUrl = URL.createObjectURL(blob);
+    const script = document.createElement('script');
+    script.src = blobUrl;
+    //script.onload = () => URL.revokeObjectURL(blobUrl);
+    document.body.appendChild(script);
+  }
 }
