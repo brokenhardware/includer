1d6b7308fc9c9372c09f8836013356e4547db6b9
brokenhardware <brokenhardware@proton.me>
2025-06-27 18:07:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //import { verifiedFetch } from 'https://esm.sh/@helia/verified-fetch?bundle-deps'
-import { verifiedFetch } from 'https://app.unpkg.com/@helia/verified-fetch@3.2.0/files/dist/index.min.js'
+import { verifiedFetch } from 'https://unpkg.com/@helia/verified-fetch/dist/index.min.js'
 
 
 const hashString = window.location.hash.substring(1);
