4ffe4526cf691c80bd183a815a7c2ab603558745
brokenhardware <brokenhardware@proton.me>
2025-06-27 18:13:40 +0300

    Create verified-fetch.js

---
+++ libs/verified-fetch.js
@@ -0,0 +1,6 @@
+/* esm.sh - @helia/verified-fetch@3.2.0 */
+import "/node/buffer.mjs";
+import "/node/fs.mjs";
+import "/node/process.mjs";
+import "/node/stream.mjs";
+export * from "/@helia/verified-fetch@3.2.0/es2022/verified-fetch.bundle.mjs";
